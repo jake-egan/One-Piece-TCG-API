3f6794cea8a99653366385fdbc5a24272d514e28
jake-egan <jakeegan0203@gmail.com>
2025-04-03 02:06:40 +0100

    Created React Project

--- BackEnd/One Piece TCG API/One Piece TCG API/Program.cs
+++ BackEnd/One Piece TCG API/One Piece TCG API/Program.cs
@@ -23,10 +23,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://0.0.0.0:5000");
